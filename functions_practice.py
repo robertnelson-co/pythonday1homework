a364d865fff0f01fa247b7dcb1a38f2e366b7676
Robert <127267616+robertnelson-co@users.noreply.github.com>
2023-10-18 19:04:38 -0400

    Add files via upload

---
+++ functions_practice.py
@@ -0,0 +1,15 @@
+def hello:
+    print("Hello World!")
+
+def pack(arg1, arg2, arg3):
+    # create a list with the arguments as items
+    result_list = [arg1, arg2, arg3]
+    return result_list
+
+def eat_lunch(food_list):
+    if not food_list:
+        print("My lunchbox is empty!")
+    elif:
+        print("First I eat {food_list[0]}")
+    else:
+        print("NExt I eat {food_list[i]}")
